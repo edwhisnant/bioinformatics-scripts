0d9f5c2da01fdc720ba8d9429e3ed81a6c7b69e2
Eric Whisnant <edw36@dcc-login-02.oit.duke.edu>
2025-10-08 09:50:43 -0400

    Update on 2025-10-08 09:50:43

---
+++ misc/update_git_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to your scripts directory
+cd /hpc/group/bio1/ewhisnant/comp-genomics/scripts || exit 1
+
+# Stage all changes (except files ignored by .gitignore)
+git add .
+
+# Commit with a timestamped message
+git commit -m "Update on $(date '+%Y-%m-%d %H:%M:%S')" 
+
+# Pull remote changes to avoid conflicts
+git pull origin main
+
+# Push your changes
+git push origin main
